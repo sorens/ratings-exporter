20c2612333ed32ab34c5ff62bfd68c73cc9d0ade
Steve Orens <steve@orens.com>
2011-12-21 09:34:45 -0800

    add additional active_admin scopes for titles

--- app/admin/titles.rb
+++ app/admin/titles.rb
@@ -2,11 +2,15 @@ ActiveAdmin.register Title do
   
   scope :all, :default => true
   scope :fivedaysold
+  scope :unfinished
+  scope :finished
   
   filter :name
   filter :year
   filter :url
   filter :rating
+  filter :exported
+  filter :user_id
   filter :netflix_type
   filter :viewed_date
   filter :created_at
--- app/models/title.rb
+++ app/models/title.rb
@@ -1,4 +1,6 @@
 class Title < ActiveRecord::Base
   
   scope :fivedaysold, where( 'created_at < ?', 5.days.ago )
+  scope :unfinished, where( 'exported = 0' )
+  scope :finished, where( 'exported = 1' )
 end
