bc981836ecf527a0ab9fa763da8b128db614cac8
Steve Orens <steve@orens.com>
2011-12-26 13:33:22 -0800

    queue a job to download the 20 latest ratings. fixes #1

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -62,6 +62,7 @@ private
         user_feed.extract
         DownloadRentalsJob.enqueue( "shipped", user_feed.shipped, token, secret, user_id )
         DownloadRentalsJob.enqueue( "watched", user_feed.watched, token, secret, user_id )
+        DownloadRentalsJob.enqueue( "ratings", user_feed.ratings, token, secret, user_id )
       rescue Exceptions::OauthHelperException => e
         Rails.logger.error "failed to request [#{e.class}] for [#{e.message}]"
         msg = { :error => e.message }
