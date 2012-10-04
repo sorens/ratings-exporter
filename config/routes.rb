4c730b6644cba2e3f7a20b398037eeb27b60bd69
Steve Orens <steve@orens.com>
2012-10-04 07:20:26 -0700

    disabling the service

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,16 @@ RatingsExporter::Application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :admin_users, ActiveAdmin::Devise.config
 
-  get 'export'                  => 'export#export'
-  get 'export_callback'         => 'export#export_callback'
+  # Netflix API changed their API, disabling service
+  # get 'export'                  => 'export#export'
+  # get 'export_callback'         => 'export#export_callback'
   get 'welcome'                 => 'welcome#index'
   get 'sign_out'                => 'welcome#sign_out'
-  get 'progress'                => 'welcome#progress'
-  get 'download'                => 'welcome#download'
-  get 'continue'                => 'welcome#continue'
-  get 'ignore'                  => 'welcome#ignore'
-  get 'delete_all'              => 'welcome#delete_all'
+  # get 'progress'                => 'welcome#progress'
+  # get 'download'                => 'welcome#download'
+  # get 'continue'                => 'welcome#continue'
+  # get 'ignore'                  => 'welcome#ignore'
+  # get 'delete_all'              => 'welcome#delete_all'
   root :to => "welcome#index"
 
   match 'status', :to => proc {|env| [200, {}, ['...']]}
